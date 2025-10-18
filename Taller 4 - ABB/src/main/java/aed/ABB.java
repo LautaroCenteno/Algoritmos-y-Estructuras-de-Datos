628839c10889f9f6cd0e85490688e79982bf98d5
LautaroCenteno <lautarocenteno.lc@gmail.com>
2025-10-18 18:22:05 -0300

    talleres

---
+++ Taller 4 - ABB/src/main/java/aed/ABB.java
@@ -0,0 +1,84 @@
+package aed;
+
+import java.util.*;
+
+// Todos los tipos de datos "Comparables" tienen el método compareTo()
+// elem1.compareTo(elem2) devuelve un entero. Si es mayor a 0, entonces elem1 > elem2
+public class ABB<T extends Comparable<T>> {
+    private Nodo _raiz;
+    private int _cardinal;
+    private int _altura;
+
+    private class Nodo {
+        T valor;
+        Nodo izq;
+        Nodo der;
+        Nodo padre;
+
+        Nodo(T v) {
+            valor = v;
+            izq = null;
+            der = null;
+            padre = null;
+        }
+    }
+
+    public ABB() {
+        _raiz = null;
+        _cardinal = 0;
+        _altura = 0;
+    }
+
+    public int cardinal() {
+        return _cardinal;
+    }
+
+    public T minimo(){
+        Nodo actual = _raiz;
+        while (actual != null & actual.izq != null) {
+            actual = actual.izq;
+        }
+        return actual.valor;
+    }
+
+    public T maximo(){
+        Nodo actual = _raiz;
+        while (actual != null & actual.der != null) {
+            actual = actual.izq;
+        }
+        return actual.valor;
+    }
+
+    public void insertar(T elem){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public boolean pertenece(T elem){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public void eliminar(T elem){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public String toString(){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    public class ABB_Iterador {
+        private Nodo _actual;
+
+        public boolean haySiguiente() {            
+            throw new UnsupportedOperationException("No implementada aun");
+        }
+    
+        public T siguiente() {
+            throw new UnsupportedOperationException("No implementada aun");
+        }
+    }
+
+    public ABB_Iterador iterador() {
+        return new ABB_Iterador();
+    }
+
+}
